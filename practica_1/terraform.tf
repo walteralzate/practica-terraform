4f76317f812fdc971d4d5f2bb86468a26821a352
walteralzate <133885722+walteralzate@users.noreply.github.com>
2023-05-29 09:07:59 -0500

    Update terraform.tf

--- practica_1/terraform.tf
+++ practica_1/terraform.tf
@@ -1,4 +1,4 @@
 resource "local_file" "productos" {
-  content  = "Lista de productos para el mes proximo"
+  content  = "Lista de productos para el mes proximo del 2023"
   filename  = "productos.txt"
 }
--- practica_5/providers.tf
+++ practica_5/providers.tf
@@ -5,7 +5,7 @@ terraform {
       version = ">=4.36.0, <4.47.0, !=4.43.0"
     }
   }
-  required_version = "~>1.3.0"
+  required_version = ">=1.3.0"
 }
 
 provider "aws" {
